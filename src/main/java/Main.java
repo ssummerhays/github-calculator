e922cbea2dc4169bf417b35b2de223c6ec408f86
ssummerhays <spencersummerhays3@gmail.com>
2025-02-18 00:25:05 -0700

    Corrected final version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
     Calculator calculator = new Calculator();
-    String[] commands = {
-            "add 5 3",
-            "subtract 10 4",
-            "multiply 7 6",
-            "divide 8 2",
-            "fibonacci 6",
-            "binary 10",
-            "exit"
-    };
-
-    for (String command : commands) {
-      System.out.println("Enter a command: \n" + command);
+    while (true) {
+      System.out.println("Enter a command: ");
+      String command = scanner.nextLine();
       String[] parts = command.split(" ");
 
       if (command.equalsIgnoreCase("exit")) {
@@ -23,41 +17,41 @@ public class Main {
       switch (parts[0].toLowerCase()) {
         // add
         case "add":
-          int addA = Integer.parseInt(parts[1]);
-          int addB = Integer.parseInt(parts[2]);
+          int addA=Integer.parseInt(parts[1]);
+          int addB=Integer.parseInt(parts[2]);
           System.out.println("Result: " + calculator.add(addA, addB));
           break;
 
-        // subtract
+          // subtract
         case "subtract":
-          int subA = Integer.parseInt(parts[1]);
-          int subB = Integer.parseInt(parts[2]);
+          int subA=Integer.parseInt(parts[1]);
+          int subB=Integer.parseInt(parts[2]);
           System.out.println("Result: " + calculator.subtract(subA, subB));
           break;
 
-        // multiply
+          // multiply
         case "multiply":
-          int mulA = Integer.parseInt(parts[1]);
-          int mulB = Integer.parseInt(parts[2]);
+          int mulA=Integer.parseInt(parts[1]);
+          int mulB=Integer.parseInt(parts[2]);
           System.out.println("Result: " + calculator.multiply(mulA, mulB));
           break;
 
         // divide
         case "divide":
-          int divA = Integer.parseInt(parts[1]);
-          int divB = Integer.parseInt(parts[2]);
+          int divA=Integer.parseInt(parts[1]);
+          int divB=Integer.parseInt(parts[2]);
           System.out.println("Result: " + calculator.divide(divA, divB));
           break;
 
-        // fibonacci
+          // fibonacci
         case "fibonacci":
-          int fibN = Integer.parseInt(parts[1]);
+          int fibN=Integer.parseInt(parts[1]);
           System.out.println("Fibonacci " + fibN + ": " + calculator.fibonacciNumberFinder(fibN));
           break;
 
-        // binary
+          // binary
         case "binary":
-          int binN = Integer.parseInt(parts[1]);
+          int binN=Integer.parseInt(parts[1]);
           System.out.println("Binary of " + binN + ": " + calculator.intToBinaryNumber(binN));
           break;
       }
