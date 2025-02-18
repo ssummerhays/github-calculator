be2c70c614b0ba7e71d5306186e77b91d33046f4
ssummerhays <spencersummerhays3@gmail.com>
2025-02-17 22:24:06 -0700

    Wrote createUniqueID method

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -71,7 +71,8 @@ class Calculator {
     if you run this function twice with the same String input, it must return 2 unique String IDs
      */
     String createUniqueID(String n){
-        return null;
+        long time = System.nanoTime();
+        return n + time;
     }
 
 
