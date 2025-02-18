0d4b78ab761b1604b3158e1849a71f91d9f1671e
ssummerhays <spencersummerhays3@gmail.com>
2025-02-17 23:37:43 -0700

    added comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,35 +15,41 @@ public class Main {
       }
 
       switch (parts[0].toLowerCase()) {
+        // add
         case "add":
           int addA=Integer.parseInt(parts[1]);
           int addB=Integer.parseInt(parts[2]);
           System.out.println("Result: " + calculator.add(addA, addB));
           break;
 
+          // subtract
         case "subtract":
           int subA=Integer.parseInt(parts[1]);
           int subB=Integer.parseInt(parts[2]);
           System.out.println("Result: " + calculator.subtract(subA, subB));
           break;
 
+          // multiply
         case "multiply":
           int mulA=Integer.parseInt(parts[1]);
           int mulB=Integer.parseInt(parts[2]);
           System.out.println("Result: " + calculator.multiply(mulA, mulB));
           break;
 
+        // divide
         case "divide":
           int divA=Integer.parseInt(parts[1]);
           int divB=Integer.parseInt(parts[2]);
           System.out.println("Result: " + calculator.divide(divA, divB));
           break;
 
+          // fibonacci
         case "fibonacci":
           int fibN=Integer.parseInt(parts[1]);
           System.out.println("Fibonacci " + fibN + ": " + calculator.fibonacciNumberFinder(fibN));
           break;
 
+          // binary
         case "binary":
           int binN=Integer.parseInt(parts[1]);
           System.out.println("Binary of " + binN + ": " + calculator.intToBinaryNumber(binN));
