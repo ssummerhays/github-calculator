2510e50eec9b9fa8d829c9d0c2ce408be70f716b
ssummerhays <spencersummerhays3@gmail.com>
2025-02-18 00:07:27 -0700

    retry scanner

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,15 +2,16 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-
+    Scanner scanner = new Scanner(System.in);
     Calculator calculator = new Calculator();
-
+    while (true) {
       System.out.println("Enter a command: ");
-      String command = "add 1 3";
+      String command = scanner.nextLine();
       String[] parts = command.split(" ");
 
       if (command.equalsIgnoreCase("exit")) {
         System.out.println("Exiting program.");
+        break;
       }
 
       switch (parts[0].toLowerCase()) {
@@ -53,7 +54,7 @@ public class Main {
           int binN=Integer.parseInt(parts[1]);
           System.out.println("Binary of " + binN + ": " + calculator.intToBinaryNumber(binN));
           break;
-
+      }
     }
   }
 }
