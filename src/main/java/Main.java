bc9cc69a5b0fb923b7deb4364b06962ae480e095
ssummerhays <spencersummerhays3@gmail.com>
2025-02-17 23:59:32 -0700

    tes the system

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,59 +2,6 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    Calculator calculator = new Calculator();
-    while (true) {
-      System.out.println("Enter a command: ");
-      String command = scanner.nextLine();
-      String[] parts = command.split(" ");
-
-      if (command.equalsIgnoreCase("exit")) {
-        System.out.println("Exiting program.");
-        break;
-      }
-
-      switch (parts[0].toLowerCase()) {
-        // add
-        case "add":
-          int addA=Integer.parseInt(parts[1]);
-          int addB=Integer.parseInt(parts[2]);
-          System.out.println("Result: " + calculator.add(addA, addB));
-          break;
-
-          // subtract
-        case "subtract":
-          int subA=Integer.parseInt(parts[1]);
-          int subB=Integer.parseInt(parts[2]);
-          System.out.println("Result: " + calculator.subtract(subA, subB));
-          break;
-
-          // multiply
-        case "multiply":
-          int mulA=Integer.parseInt(parts[1]);
-          int mulB=Integer.parseInt(parts[2]);
-          System.out.println("Result: " + calculator.multiply(mulA, mulB));
-          break;
-
-        // divide
-        case "divide":
-          int divA=Integer.parseInt(parts[1]);
-          int divB=Integer.parseInt(parts[2]);
-          System.out.println("Result: " + calculator.divide(divA, divB));
-          break;
-
-          // fibonacci
-        case "fibonacci":
-          int fibN=Integer.parseInt(parts[1]);
-          System.out.println("Fibonacci " + fibN + ": " + calculator.fibonacciNumberFinder(fibN));
-          break;
-
-          // binary
-        case "binary":
-          int binN=Integer.parseInt(parts[1]);
-          System.out.println("Binary of " + binN + ": " + calculator.intToBinaryNumber(binN));
-          break;
-      }
-    }
+    System.out.println("Testing docker");
   }
 }
